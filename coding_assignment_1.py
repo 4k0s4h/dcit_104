59566797119bf947f793eda55f36657dd3554bc9
4k0s4h <101297641+4k0s4h@users.noreply.github.com>
2022-08-07 19:11:30 +0000

    Initial commit

--- coding_assignment_1.py
+++ coding_assignment_1.py
@@ -1,6 +1,18 @@
 """
+This assignment was done and submitted by
 Yaw Akosah Asare
 10946857
-This assignment was done and submitted on my own.
 """
-# this is just a comment 
+def sum_ofPrimes():
+    sum = 0
+
+    for num in range(2, 10000):
+        i = 2
+        for i in range(2, num):
+            if num % i == 0:
+                i = num
+                break
+        if i is not num:
+            sum += num
+    print(sum)
+sum_ofPrimes()
