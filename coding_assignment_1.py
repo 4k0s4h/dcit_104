d0d929c8fe24c7a3fb550a3c4b9344e25c138a39
4k0s4h <101297641+4k0s4h@users.noreply.github.com>
2022-08-07 22:43:11 +0000

    Converted sum to Average

--- coding_assignment_1.py
+++ coding_assignment_1.py
@@ -3,10 +3,10 @@ This assignment was completed and submitted by
 Yaw Akosah Asare
 10946857
 """
-def sum_ofPrimes():
+def avg_ofPrimes():
     sum = 0
 
-    for num in range(2, 10000):
+    for num in range(2, 10):
         i = 2
         for i in range(2, num):
             if num % i == 0:
@@ -14,5 +14,5 @@ def sum_ofPrimes():
                 break
         if i is not num:
             sum += num
-    print(sum)
-sum_ofPrimes()
+    print(sum / i)
+avg_ofPrimes()
