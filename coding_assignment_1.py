d15b9abf581ef4f0e0a43cbc75a8ce00ec2e3a45
4k0s4h <101297641+4k0s4h@users.noreply.github.com>
2022-08-08 12:16:20 +0000

    Minor fixes

--- coding_assignment_1.py
+++ coding_assignment_1.py
@@ -8,10 +8,13 @@ Yaw Akosah Asare
 def avg_ofPrimes():
     sum = 0
     count = 0
-    for number in range(2, 10000):
-        for i in range(2, number + 1):
-            if number % i == 0:
-                sum = sum + i
-        count = count + 1
+    for number in range(1, 10000):
+        if number > 1:
+            for i in range(2, number):
+                if number % i == 0:
+                    break
+                else:
+                    sum = sum + i
+            count = count + 1
     print(sum / (count + 1))
 avg_ofPrimes()
